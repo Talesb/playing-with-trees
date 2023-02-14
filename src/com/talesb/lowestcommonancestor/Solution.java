2fe9326ed7f252cb13eb90dd303f3e5ef8ee1281
talesb <tales.batista@temasistemas.com.br>
2023-02-14 00:46:27 -0300

    Code to find commom ancestor node in a bynary search tree

---
+++ src/com/talesb/lowestcommonancestor/Solution.java
@@ -0,0 +1,21 @@
+package com.talesb.lowestcommonancestor;
+
+import com.talesb.TreeNode;
+
+public class Solution {
+
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+		 if(p.val<root.val&&q.val<root.val) {
+			 return lowestCommonAncestor(root.left, p, q);
+		 }
+		 else if(p.val>root.val&&q.val>root.val) {
+			 return lowestCommonAncestor(root.right, p, q);
+		 }else {
+			 return root; 
+		 }
+		 	
+		
+	}
+
+}
