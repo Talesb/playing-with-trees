c2ab97e98cd36d19ae098ad5345eba67bdf5d957
talesb <tales.batista@temasistemas.com.br>
2023-02-23 00:10:31 -0300

    Solving some exercises from Sao Paulo University Math Institute about Binary Trees

---
+++ src/com/talesb/binarytree/exercise1/Solution.java
@@ -0,0 +1,47 @@
+package com.talesb.binarytree.exercise1;
+
+import com.talesb.TreeNode;
+
+//PT-BR  Escreva uma função recursiva que receba uma árvore binária ab e um número x e remova da árvore todas as folhas que tenham item igual a x.
+//EN-US  Write a recursive function that takes a binary tree ab and a number x and removes from the tree all leaves that have value equal to x.
+
+public class Solution {
+
+	public static void main(String[] args) {
+	
+		TreeNode node5 = new TreeNode(5);
+		TreeNode node7 = new TreeNode(7);
+		TreeNode node6 = new TreeNode(6,node5,node7);
+		TreeNode node9 = new TreeNode(9);
+		TreeNode node11 = new TreeNode(11);
+		TreeNode node10 = new TreeNode(10,node9,node11);
+		
+		TreeNode exampleNode = new TreeNode(8,node6,node10);
+
+		removeNodeFromValue(exampleNode, 6);
+		
+		//Just added to debug purpose
+		System.out.println(exampleNode);
+		
+		
+	}
+	
+	
+	public static void removeNodeFromValue(TreeNode node,int value) {
+		if(node!= null) {
+			if(node.left!= null && node.left.val == value) {
+				node.left = null;
+			}
+			
+			if(node.right!= null && node.right.val == value) {
+				node.right = null;
+			}
+			removeNodeFromValue(node.left, value);
+			removeNodeFromValue(node.right, value);
+		}
+	}
+	
+	
+	
+	
+}
